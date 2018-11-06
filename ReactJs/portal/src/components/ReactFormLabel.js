ae36169756992e3e7ba0c51415e35f587bff7e15
Rafael Soares <rafaelsoaresdes@gmail.com>
2018-11-05 22:24:04 -0200

    update status

--- ReactJs/portal/src/components/ReactFormLabel.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default class ReactFormLabel extends React.Component {
-  render() {
-    return (
-        <label className='form-label' htmlFor={this.props.htmlFor}>{this.props.title}</label>
-    );
-  }
-}
